f229813a1408b02cf9f5e70f714e5be9f5c4c62e
Mark Stier <stier020@Marks-MacBook-Pro.local>
2015-11-20 12:31:15 -0600

    make only virtual frames

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict"
 
 var Worker = require("basic-distributed-computation").Worker;
-var Jimp = require("jimp");
+var VirtualFrame = require("virtual-frames-edc");
 
 var scaleMatch = /_scale\/{([1-9](\.[0-9][0-9]?[0-9]?)?)}/;
 var rotateMatch = /_rotate\/{([0-9](\.[0-9][0-9]?[0-9]?)?)}/;
@@ -9,61 +9,9 @@ var shiftXMatch = /_shift_x\/{([1-9][0-9]*)}/;
 var shiftYMatch = /_shift_y\/{([1-9][0-9]*)}/;
 var minXMatch = /_min_x\/{([1-9][0-9]*)}/;
 var minYMatch = /_min_y\/{([1-9][0-9]*)}/;
-var resizeToMin = /_resize_to_min\/{true}/
-
-class VirtualFrame {
-  constructor(maxX, maxY, xOffset, yOffset, width, height, minX, minY, resize){
-    this.maxX = maxX;
-    this.maxY = maxY;
-    this.xOffset = xOffset;
-    this.yOffset = yOffset;
-    this.width = width;
-    this.height = height;
-    this.minX = minX;
-    this.minY = minY;
-    this.resize = resize;
-  }
-
-  shiftX(shiftDistance){
-    if(this.maxX > this.xOffset + this.width + shiftDistance){
-      return new VirtualFrame(this.maxX, this.maxY, this.xOffset + shiftDistance, this.yOffset, this.width, this.height, this.minX, this.minY, this.resize);
-    } else {
-      return null;
-    }
-  }
-
-  shiftY(shiftDistance){
-    if(this.maxY > this.yOffset + this.height + shiftDistance){
-      return new VirtualFrame(this.maxX, this.maxY, this.xOffset, this.yOffset + shiftDistance, this.width, this.height, this.minX, this.minY, this.resize);
-    } else {
-      return null;
-    }
-  }
-
-  scale(factor){
-    if(this.maxY > this.yOffset + this.height*factor && this.maxX > this.xOffset + this.width*factor){
-      return new VirtualFrame(this.maxX, this.maxY, this.xOffset, this.yOffset, Math.floor(this.width * factor), Math.floor(this.height * factor), this.minX, this.minY, this.resize);
-    } else {
-      return null;
-    }
-  }
-
-  makeConcrete(image, resize){
-    return new Promise( (resolve, reject) => {
-      var img = image.crop(this.xOffset, this.yOffset, this.width, this.height)
-      if(this.resize){
-        img = img.resize(this.minX, this.minY)
-      }
-        img.quality(75)
-        .getBuffer(Jimp.MIME_JPEG, (err, buff) => {
-          resolve({
-            image:buff,
-            frameData: this
-          });
-        });
-    });
-  }
-}
+var resizeToMin = /_resize_to_min\/{true}/;
+var heightMatch = /_height\/{([1-9][0-9]*)}/;
+var widthMatch = /_width\/{([1-9][0-9]*)}/;
 
 class CreateFrames extends Worker {
   constructor(parent){
@@ -78,66 +26,83 @@ class CreateFrames extends Worker {
     var currentPath = req.paths[req.currentIdx];
     var scale = scaleMatch.exec(currentPath);
     scale = scale ? parseFloat(scale[1]) : 2;
+    if(inputVal.scale){
+        scale = inputVal.scale;
+    }
     var rotate = rotateMatch.exec(currentPath);
     rotate = rotate ? parseFloat(rotate[1]) : 0;
+    if(inputVal.rotate){
+        rotate = inputVal.rotate;
+    }
     var shiftX = shiftXMatch.exec(currentPath);
     shiftX = shiftX ? parseInt(shiftX[1]) : 1;
+    if(inputVal.shiftX){
+        shiftX = inputVal.shiftX;
+    }
     var shiftY = shiftYMatch.exec(currentPath);
     shiftY = shiftY ? parseInt(shiftY[1]) : 1;
+    if(inputVal.shiftY){
+        shiftY = inputVal.shiftY;
+    }
     var minX = minXMatch.exec(currentPath);
     minX = minX ? parseInt(minX[1]) : 32;
+    if(inputVal.minX){
+        minX = inputVal.minX;
+    }
     var minY = minYMatch.exec(currentPath);
-    minY = minY ? parseInt(minY[1]) : 32
+    minY = minY ? parseInt(minY[1]) : 32;
+    if(inputVal.minY){
+        minY = inputVal.minY;
+    }
     var resize = resizeToMin.exec(currentPath);
     resize = resize ? true : false;
+    if(inputVal.resize){
+        resize = inputVal.resize;
+    }
+    var height = heightMatch.exec(currentPath);
+    height = height ? parseInt(height) : 1;
+    if(inputVal.height){
+        height = inputVal.height;
+    }
+    var width = widthMatch.exec(currentPath);
+    width = width ? pathInt(width) : false;
+    if(inputVal.width){
+        width = inputVal.width;
+    }
+    var frames = [];
 
-    Jimp.read(inputVal).then((image) => {
-      var height = image.bitmap.height;
-      var width = image.bitmap.width;
-      var frames = [];
-      var initFrame = new VirtualFrame(width, height, 0, 0, minX, minY, resize);
-      var newFrame = initFrame;
-      var curScale = 1;
+    var initFrame = new VirtualFrame(width, height, 0, 0, minX, minY, resize);
+    var newFrame = initFrame;
+    var curScale = 1;
 
-      while(newFrame){
-        newFrame = newFrame.scale(curScale);
-        if(newFrame){
-          frames.push(newFrame);
-        }
-        var downFrame = newFrame;
-        while(downFrame){
-          var rightFrame = downFrame;
-          while(rightFrame){
-            rightFrame = rightFrame.shiftX(shiftX);
-            if(rightFrame){
-              frames.push(rightFrame);
-            }
-          }
-          downFrame = downFrame.shiftY(shiftY);
-          if(downFrame){
-            frames.push(downFrame);
-          }
-        }
-
-        curScale *= scale;
+    while(newFrame){
+      newFrame = newFrame.scale(curScale);
+      if(newFrame){
+        frames.push(newFrame);
       }
-      return Promise.all(
-        frames.map((frame) => {
-          return frame.makeConcrete(image, resize, minX, minY);
-        })
-      );
-    }).then((frames) => {
-      if(outputKey){
-        req.body[outputKey] = frames;
-      } else {
-        req.body = frames
+      var downFrame = newFrame;
+      while(downFrame){
+        var rightFrame = downFrame;
+        while(rightFrame){
+          rightFrame = rightFrame.shiftX(shiftX);
+          if(rightFrame){
+            frames.push(rightFrame);
+          }
+        }
+        downFrame = downFrame.shiftY(shiftY);
+        if(downFrame){
+          frames.push(downFrame);
+        }
       }
-      req.next();
-    }).catch((err) => {
-      req.status(err).next();
-    });
 
-  }
+      curScale *= scale;
+    }
+    if(outputKey){
+      req.body[outputKey] = frames;
+    } else {
+      req.body = frames;
+    }
+    req.next();
 }
 
 module.exports = CreateFrames;
